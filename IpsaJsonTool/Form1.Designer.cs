fb4988b13a82e9f808ed8547a7308f82d2ed8e14
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2023-12-05 08:16:07 +0100

    ipsajson impl

--- IpsaJsonTool/Form1.Designer.cs
+++ IpsaJsonTool/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importIpsaJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importMojangJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportIpsaJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportMojangJsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(212, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importIpsaJsonToolStripMenuItem,
+            this.exportIpsaJsonToolStripMenuItem,
+            this.importMojangJsonToolStripMenuItem,
+            this.exportMojangJsonToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importIpsaJsonToolStripMenuItem
+            // 
+            this.importIpsaJsonToolStripMenuItem.Name = "importIpsaJsonToolStripMenuItem";
+            this.importIpsaJsonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importIpsaJsonToolStripMenuItem.Text = "Import IpsaJson";
+            // 
+            // importMojangJsonToolStripMenuItem
+            // 
+            this.importMojangJsonToolStripMenuItem.Name = "importMojangJsonToolStripMenuItem";
+            this.importMojangJsonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.importMojangJsonToolStripMenuItem.Text = "Import MojangJson";
+            // 
+            // exportIpsaJsonToolStripMenuItem
+            // 
+            this.exportIpsaJsonToolStripMenuItem.Name = "exportIpsaJsonToolStripMenuItem";
+            this.exportIpsaJsonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportIpsaJsonToolStripMenuItem.Text = "Export IpsaJson";
+            // 
+            // exportMojangJsonToolStripMenuItem
+            // 
+            this.exportMojangJsonToolStripMenuItem.Name = "exportMojangJsonToolStripMenuItem";
+            this.exportMojangJsonToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportMojangJsonToolStripMenuItem.Text = "Export MojangJson";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importIpsaJsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportIpsaJsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importMojangJsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportMojangJsonToolStripMenuItem;
     }
 }
 
---
+++ IpsaJsonTool/ipsajson/IpsaJson.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IpsaJsonTool.ipsajson
+{
+    // IpsaJson v1.0
+    // by DEJVOSS Productions
+    // use for Codex-Ipsa Launcher
+
+    public class IpsaJson
+    {
+        public string game { get; set; }
+        public string version { get; set; }
+        public string url { get; set; }
+        public int size { get; set; }
+        public int java { get; set; }
+        public string cmdBef { get; set; }
+        public string cmdAft { get; set; }
+        public string defRes { get; set; }
+        public bool srvJoin { get; set; }
+        public IpsaJsonAssets assets { get; set; }
+        public string logging { get; set; }
+        public string classpath { get; set; }
+        public IpsaJsonSupplement[] supplement { get; set; }
+        public IpsaJsonLibraries[] libraries { get; set; }
+    }
+
+    public class IpsaJsonAssets
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public int size { get; set; }
+        public int fileSize { get; set; }
+    }
+
+    public class IpsaJsonLibraries
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public int size { get; set; }
+        public bool extract { get; set; }
+    }
+
+    public class IpsaJsonSupplement
+    {
+        public string url { get; set; }
+        public string path { get; set; }
+        public int name { get; set; }
+        public bool renew { get; set; }
+    }
+}
